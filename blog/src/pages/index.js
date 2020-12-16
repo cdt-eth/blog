1074b145b4f441c7a203723714ed1d049d09ad3a
christian <christiandavidphoto@gmail.com>
2020-12-16 15:30:23 -0500

    home updates

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -7,27 +7,27 @@ import Button from "../components/button"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Gatsby Starter Personal Website"
+    const siteTitle = "Christian Turner's Dev Blog"
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
+      <Layout location={this.props.location} title={siteTitle} >
         <SEO
           title="Home"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
+        <img style={{ margin: 0, height:250 }} src="./christian.jpg" alt="Gatsby Scene" />
         <h1>
           Hey there{" "}
           <span role="img" aria-label="wave emoji">
             ⚡️
           </span>
         </h1>
-        <p>Welcome to my blog! You are on your home page.</p>
+        <p>Welcome to my blog! </p>
+          {/* You are on your home page.</p> */}
         <p>
-          This starter comes out of the box with styled components and Gatsby's
-          default starter blog running on Netlify CMS.
+          I'm a programmer interested in iOS development and human-computer interaction. I'm in <a href="https://omscs.gatech.edu/" target="_blank">grad school at Georgia Tech</a> getting a Master's in CS with a concentation in machine learning. I'm obssessed with self-learing and furthering my education.
         </p>
-        <p>Now go build something great!</p>
+        <p>This blog serves as a record of my projects, accomplishments, and learnings.</p>
         <Link to="/blog/">
           <Button marginTop="35px">Go to Blog</Button>
         </Link>
