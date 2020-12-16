be67aef930ea324861255ce0b49f672a5eaee763
christian <christiandavidphoto@gmail.com>
2020-12-16 11:48:14 -0500

    home text change test

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -17,12 +17,12 @@ class IndexPage extends React.Component {
         />
         <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
         <h1>
-          Hey people{" "}
+          Hey there{" "}
           <span role="img" aria-label="wave emoji">
-            👋
+            ⚡️
           </span>
         </h1>
-        <p>Welcome to your new Gatsby website. You are on your home page.</p>
+        <p>Welcome to my blog! You are on your home page.</p>
         <p>
           This starter comes out of the box with styled components and Gatsby's
           default starter blog running on Netlify CMS.
