7f6ab4a237f0e361af434591070702f503d6e45e
christianturner <christiandavidphoto@gmail.com>
2021-03-22 15:14:25 -0400

    site maxwidth increased

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -111,7 +111,8 @@ class Layout extends React.Component {
           style={{
             marginLeft: `auto`,
             marginRight: `auto`,
-            maxWidth: rhythm(24),
+            maxWidth: rhythm(35),
+            // maxWidth: rhythm(44),
             padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
           }}
         >
