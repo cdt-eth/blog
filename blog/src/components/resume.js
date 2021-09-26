7bb5bae1d8f82c8d685f018f5d1e5aa9ecdc2e73
christianturner <christiandavidphoto@gmail.com>
2021-09-25 22:20:38 -0400

    broke out resume to component

---
+++ blog/src/components/resume.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+const Resume = () => {
+  return (
+    <div>
+      <p>
+        View my{" "}
+        <a
+          href="https://drive.google.com/file/d/1frZ_8FK8C_CJ1E77eeGwoc88sEx7I2KW/view"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Resume
+        </a>
+      </p>
+    </div>
+  )
+}
+
+export default Resume
